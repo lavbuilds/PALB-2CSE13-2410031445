fc0b3a18a5ea3d48695e1bd20a8a2e8e110dc150
Lavanya Jangid <lavanyajangid875@gmail.com>
2026-02-26 15:32:16 +0530

    Add files via upload

---
+++ HOMEWORK-1/problem1.py
@@ -0,0 +1,8 @@
+def reverse_array(arr):
+    start, end = 0, len(arr) - 1
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+    return arr
+
